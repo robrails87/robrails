1e93fc6dff89e711bf02217e618f512321cf97c8
Robert Pliszka <robrails87@gmail.com>
2013-03-25 22:41:22 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robrails::Application.config.secret_token = 'f9fbdde3698da983c14148a5ea6bfd5481eeeb3b68ce0f4931c767a24f6a77f776673cfb2869a4806b588b420afff9d5e58bae7fbbd3b7372d31ea807e8a1072'
